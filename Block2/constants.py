47d89507cdfa2d15caa6910c7e7b11bfa81ad6a3
Denis <gleb.perepuhov@mail.ru>
2020-12-17 22:22:13 +0300

    Block2Tasks
    This is tasks from block 2 (1 and 2 together, 3 separate)

---
+++ Block2/constants.py
@@ -0,0 +1,6 @@
+muE = 3986004415e5 # Standard gravitational parameter of Earth; m^3/s^2
+RE = 6371e3 # Earths radius; m
+ISS_periapsis = 337e3 # ISS periapsis height; m
+ISS_apoapsis = 430e3 # ISS apoapsis height; m
+ISS_speed = 7600 # ISS mean speed; m/s
+max_to_zero = 1e-6 # If a value is less than this it will be treated as zero
