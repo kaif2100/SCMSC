75d94e196d190fe0f44162e3dfad76586a9d107e
Denis <gleb.perepuhov@mail.ru>
2021-01-28 18:07:06 +0300

    First task from block 3

---
+++ Block3/constants.py
@@ -0,0 +1,8 @@
+muE = 3986004415e5 # Standard gravitational parameter of Earth; m^3/s^2
+mmE = 8.3e22 # Earths magentic moment; A*m^2
+m0 = 1.2566370614e-6 # Magnetic constants; H/A^2
+RE = 6371e3 # Earths radius; m
+ISS_periapsis = 337e3 # ISS periapsis height; m
+ISS_apoapsis = 430e3 # ISS apoapsis height; m
+ISS_speed = 7600 # ISS mean speed; m/s
+max_to_zero = 1e-6 # If a value is less than this it will be treated as zero
